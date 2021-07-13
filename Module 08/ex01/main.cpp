660745f78bf89619839a84f6156ee89e2d641d83
Issam Ounejjar <iounejja@e1r6p14.1337.ma>
2021-07-13 20:15:01 +0100

    CPP Module08

--- Module 08/ex01/main.cpp
+++ Module 08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iounejja <iounejja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 14:39:23 by iounejja          #+#    #+#             */
-/*   Updated: 2021/07/11 10:53:40 by iounejja         ###   ########.fr       */
+/*   Updated: 2021/07/13 13:39:02 by iounejja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,16 @@
 #include "span.hpp"
 
 int		main(void) {
-	// Span sp = Span(5);
+	Span sp = Span(5);
 
-	// sp.addNumber(5);
-	// sp.addNumber(3);
-	// sp.addNumber(17);
-	// sp.addNumber(9);
-	// sp.addNumber(11);
+	sp.addNumber(5);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
 
-	std::vector<int>	lst;
-
-	lst.push_back(91);
-	lst.push_back(122);
-	lst.push_back(3546);
-	lst.push_back(41);
-	lst.push_back(54);
-
-	// std::sort(lst.begin(), lst.end());
-	// for (std::vector<int>::iterator i = lst.begin(); i != lst.end(); i++)
-	// 	std::cout << *i << std::endl;
-	Span s = Span(5);
-
-	s.addNumber(lst.begin(), lst.end());
-
-	std::cout << s.shortestSpan() << std::endl;
-	std::cout << s.longestSpan() << std::endl;
-
-	// Span s = Span(9);
-
-	// s.addNumber(13);
-	// s.addNumber(894);
-	// s.addNumber(133);
-	// s.addNumber(7897);
-	// s.addNumber(0);
-	// s.addNumber(47);
-	// s.addNumber(741);
-	// s.addNumber(4);
-	// s.addNumber(894);
-
-	// try {
-	// 	std::cout << s.shortestSpan() << std::endl;
-	// 	std::cout << s.longestSpan() << std::endl;
-	// }
-	// catch (std::exception & e) {
-	// 	std::cout << e.what() << std::endl;
-	// }
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 
 	return (0);
 }
--- Module 08/ex02/mutantstack.cpp
+++ Module 08/ex02/mutantstack.cpp
@@ -6,18 +6,8 @@
 /*   By: iounejja <iounejja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 15:41:09 by iounejja          #+#    #+#             */
-/*   Updated: 2021/07/09 14:28:56 by iounejja         ###   ########.fr       */
+/*   Updated: 2021/07/13 13:50:40 by iounejja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
-
-// template <typename T>
-// MutantStack<T>::iterator::iterator(void) {
-// 	return :
-// }
-
-// template <typename T>
-// MutantStack<T>::iterator::iterator(void) {
-// 	return :
-// }
