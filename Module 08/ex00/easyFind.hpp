479bda0575caa4c01b290af6212f0ab13a7a0e40
Issam Ounejjar <iounejja@e1r11p3.1337.ma>
2021-09-03 13:09:05 +0100

    finishing Module08 CPP

--- Module 08/ex00/easyFind.hpp
+++ Module 08/ex00/easyFind.hpp
@@ -6,7 +6,7 @@
 /*   By: iounejja <iounejja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 14:17:20 by iounejja          #+#    #+#             */
-/*   Updated: 2021/09/01 15:34:58 by iounejja         ###   ########.fr       */
+/*   Updated: 2021/09/03 13:08:12 by iounejja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <algorithm>
 
 template<typename T>
-int		easyFind(T container, int value) {=>
+int		easyFind(T container, int value) {
 	if (std::find(container.begin(), container.end(), value) != container.end())
 		return (1);
 	return (0);
